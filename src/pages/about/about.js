44db89beedab699c1ed97cc972c61955ec52d4ec
Samuel Londoño <samuel.londono@olsoft.com.co>
2020-06-01 23:53:55 -0500

    [Fix]: Scroll End

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Nav } from '../../components'
 function About() {
+    const endRef = useRef(null)
+
+
+    useEffect(() => {
+        console.log("Into here")
+        endRef.current.scrollIntoView({ behavior: "smooth" })
+    })
+
     return (
         <div>
-            <Nav flag={1}/>
+            <Nav flag={1} />
             <div className="row m-0">
                 <div className="col-md-12" >
-                    <img src={require('../../assets/img/body.jpg')} className="img-fluid" style={{ height: "auto", width: "100%"}}/>
+                    <img src={require('../../assets/img/body.jpg')} className="img-fluid" style={{ height: "auto", width: "100%" }} />
                 </div>
                 <div className="col-md-6 p-5 d-flex justify-content-center flex-column align-items-center mt-5">
                     <h1>¿Quiénes somos?</h1>
@@ -20,10 +28,13 @@ function About() {
                     </p>
                     <button className="btn mt-3" style={{ borderRadius: 30, background: "#35b4bc", color: "white" }}>CONTÁCTENOS</button>
                 </div>
-                <div className="col-md-6 mt-5">
-                    <img src={require('../../assets/img/logo_vertical.png')} style={{ width: "100%"}}/>
+                <div className="col-md-6 mt-5"  ref={endRef}>
+                    <img src={require('../../assets/img/logo_vertical.png')} style={{ width: "100%" }} />
                 </div>
             </div>
+            <footer className="d-flex justify-content-center align-items-center p-3" style={{ background: "#35b4bc" }}>
+                <p className="p-0 m-0 text-white">Copyright © 2020 - Todos los Derechos Reservados - Diamond Pixels</p>
+            </footer>
         </div>
     );
 }
